d6ead67febea2e9c6aec83a00d61b44bae0e8f1c
DynoW <104089032+DynoW@users.noreply.github.com>
2023-07-08 20:56:34 +0300

    new file:   BanList.json 	new file:   BanList.txt 	modified:   Bot.py

--- Bot.py
+++ Bot.py
@@ -1,13 +1,23 @@
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
+import json
 import os
 
 intents = discord.Intents.default()
+intents.message_content = True
 intents.members = True
 bot = commands.Bot(command_prefix='!', description="This is a Helper Bot", intents=intents)
 
-BanList = [705541156850892841]
+def ban_check(data):
+    with open('BanList.json') as ban_file:
+        list = json.load(ban_file)
+        for banned in list["bans"]:
+            print(banned)
+            if banned["Id"] == data:
+                return True
+            else:
+                return False
 
 
 @bot.event
@@ -17,46 +27,51 @@ async def on_ready():
 
 
 @bot.command()
-async def msg(ctx, target1, message1):
-    target2 = await bot.fetch_user(target1.replace("@", "").replace("<", "").replace(">", "").replace("!", ""))
-    if ctx.author.id == BanList:
-        await ctx.channel.send("⚠You are banned!⚠")
+async def msg(ctx, target1, *message1):
+    if len(message1) == 0:
+        await ctx.channel.send("Use: `!msg [user] [message]`")
     else:
-        await target2.send(str(message1) + " ~ from " + f'''<@{ctx.author.id}>''')
-        print(str(message1) + " ~ de la " + f'''<@{ctx.author.id}>''')
+        target2 = await bot.fetch_user(target1.replace("@", "").replace("<", "").replace(">", "").replace("!", ""))
+        if ban_check(ctx.author.id) == True:
+            await ctx.channel.send("⚠️ You are banned!⚠️")
+        else:
+            await target2.send(str(message1) + " ~ DM from " + f'''<@{ctx.author.id}>''')
+            print(str(message1) + " ~ de la " + f'''<@{ctx.author.id}>''')
 
 
 @bot.command()
-async def announce(ctx, message2):
-    if ctx.author.id == BanList:
-        await ctx.channel.send("⚠You are banned!⚠")
-    elif ctx.author.guild_permissions.administrator:
-        for member1 in ctx.guild.members:
-            try:
-                member2 = await bot.fetch_user(member1.id)
-                await member2.send(str(message2) + " ~ announcement from " + f"""<@{ctx.author.id}>""")
-                print(f"""-----announcement-----:{message2} by {ctx.author.id}""")
-                break
-            except:
-                pass
+async def ann(ctx, *message2):
+    if len(message2) == 0:
+        await ctx.channel.send("Use: `!ann [user] [message]`")
     else:
-        await ctx.channel.send("⚠You don't have permision to use this command!⚠")
+        if ban_check(ctx.author.id) == True:
+            await ctx.channel.send("⚠️ You are banned!⚠️")
+        elif ctx.author.guild_permissions.administrator:
+            for member1 in ctx.guild.members:
+                try:
+                    member2 = await bot.fetch_user(member1.id)
+                    await member2.send(str(message2) + " ~ announcement from " + f"""<@{ctx.author.id}>""")
+                    print(f"""<-----announcement----->:{message2} by {ctx.author.id}""")
+                    break
+                except:
+                    pass
+        else:
+            await ctx.channel.send("⚠️ You don't have permision to use this command!⚠️")
 
 
 @bot.command()
-async def report(ctx, target3, reason):
+async def report(ctx, target3, *reason):
     print(
-        f"""-----!report!-----: {target3.replace("@", "").replace("<", "").replace(">", "").replace("!", "")} by {ctx.author.id} for {reason}""")
-
+        f"""<-----!report!----->: {target3.replace("@", "").replace("<", "").replace(">", "").replace("!", "")} by {ctx.author.id} for {reason}""")
+    ctx.channel.send("Report sent!")
 
 @bot.command()
 async def commands(ctx):
     embed = discord.Embed(title="Commands", description="for @Bot DM#6773",
                           color=discord.Color.orange())
-    embed.add_field(name="!msg", value="Send momone a message. example: !msg @DynoW#9056 You are the best!", inline=False)
-    embed.add_field(name="!announce", value="(Admin only) Announce everyone on the server about something. example: !announce Ntza", inline=False)
-    embed.add_field(name="!report", value="Send a report for an user. example: !report @BotDM scam", inline=False)
-    embed.add_field(name="!website", value="Coming Soon!", inline=False)
+    embed.add_field(name="!msg", value="Send somone a message. example: !msg @DynoW#9056 You are the best!", inline=True)
+    embed.add_field(name="!ann", value="(Admin only) Announce everyone on the server about something. example: !ann Ntza", inline=True)
+    embed.add_field(name="!report", value="Send a report for an user. example: !report @BotDM scam", inline=True)
     embed.set_footer(text="For help contact: DynoW#9056")
     await ctx.send(embed=embed)
 
