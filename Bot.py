946b3739e008fe6706538a0a73db5b1095c632bb
DynoW <104089032+DynoW@users.noreply.github.com>
2023-07-08 22:12:29 +0300

    modified:   BanList.json 	modified:   Bot.py

--- Bot.py
+++ Bot.py
@@ -7,13 +7,12 @@ import os
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
-bot = commands.Bot(command_prefix='!', description="This is a Helper Bot", intents=intents)
+bot = commands.Bot(command_prefix="!", description="This is a Helper Bot", intents=intents)
 
 def ban_check(data):
-    with open('BanList.json') as ban_file:
+    with open("BanList.json", "r") as ban_file:
         list = json.load(ban_file)
         for banned in list["bans"]:
-            print(banned)
             if banned["Id"] == data:
                 return True
             else:
@@ -23,20 +22,20 @@ def ban_check(data):
 @bot.event
 async def on_ready():
     await bot.change_presence(status=discord.Status.online, activity=discord.Game(name="!commands"))
-    print('Bot is online')
+    print("Bot is online")
 
 
 @bot.command()
 async def msg(ctx, target1, *message1):
-    if ban_check(ctx.author.id) == True:
-        await ctx.channel.send("⚠️ You are banned!⚠️")
+    if ban_check(str(ctx.author.id)) == True:
+        await ctx.channel.send("⚠️ You are banned! ⚠️")
     elif len(message1) == 0:
         await ctx.channel.send("Use: `!msg [user] [message]`")
     elif target1 == "all":
         if ctx.author.guild_permissions.administrator:
             for member1 in ctx.guild.members:
                 try:
-                    message = ' '.join(message1)
+                    message = " ".join(message1)
                     member2 = await bot.fetch_user(member1.id)
                     await member2.send(str(message) + " ~ announcement from " + f"""<@{ctx.author.id}>""")
                     print(f"""<-----announcement----->:{message} by {ctx.author.id}""")
@@ -44,16 +43,28 @@ async def msg(ctx, target1, *message1):
                 except:
                     pass
         else:
-            await ctx.channel.send("⚠️ You don't have permision to use this command!⚠️")
+            await ctx.channel.send("⚠️ You don't have permision to use this command! ⚠️")
+    elif target1 == "block" or target1 == "ban":
+        if ctx.author.guild_permissions.administrator:
+            message2 = message1[0].replace("@", "").replace("<", "").replace(">", "").replace("!", "")
+            with open("BanList.json", "r") as ban_file:
+                list = json.load(ban_file)
+            list["bans"]= list["bans"] + [{"Id": message2}]
+            with open('BanList.json', 'w') as ban_file:
+                json.dump(list, ban_file)
+        else:
+            await ctx.channel.send("⚠️ You don't have permision to use this command! ⚠️")
     else:
-        message = ' '.join(message1)
-        target2 = await bot.fetch_user(target1.replace("@", "").replace("<", "").replace(">", "").replace("!", ""))
-        await target2.send(str(message) + " ~ DM from " + f'''<@{ctx.author.id}>''')
-        print(str(message) + " ~ de la " + f'''<@{ctx.author.id}>''')
-
+        try:
+            message = " ".join(message1)
+            target2 = await bot.fetch_user(target1.replace("@", "").replace("<", "").replace(">", "").replace("!", ""))
+            await target2.send(str(message) + " ~ DM from " + f"""<@{ctx.author.id}>""")
+            print(str(message) + " ~ de la " + f"""<@{ctx.author.id}>""")
+        except:
+            await ctx.channel.send("⚠️ ERROR! ⚠️ Use: `!commands`")
 @bot.command()
 async def report(ctx, target3, *reason1):
-    reason = ' '.join(reason1)
+    reason = " ".join(reason1)
     print(
         f"""<-----!report!----->: {target3.replace("@", "").replace("<", "").replace(">", "").replace("!", "")} by {ctx.author.id} for {reason}""")
     await ctx.channel.send("Report sent!")
